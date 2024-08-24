bf6521c575de48d296f7fa12b8928c6e7e0e70df
Arun Anand <107839202+ArunAnand22@users.noreply.github.com>
2024-08-24 21:58:39 +0530

    Token in Cookies

--- client/src/app/activation/sign-in/sign-in.component.ts
+++ client/src/app/activation/sign-in/sign-in.component.ts
@@ -1,44 +1,48 @@
 import { Component, inject } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from '../../services/auth.service';
+import { CookieService } from 'ngx-cookie-service';
+
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [MatInputModule,MatButtonModule,ReactiveFormsModule],
+  imports: [MatInputModule, MatButtonModule, ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
-export class SignInComponent{
-toaster = inject(ToastrService)
-authService = inject(AuthService)
+export class SignInComponent {
+
+  toaster = inject(ToastrService)
+  authService = inject(AuthService)
+  cookieService = inject(CookieService)
 
 
-formBuilder = inject(FormBuilder);
-loginForm = this.formBuilder.group({
-  email:['',[Validators.required,Validators.email]],
-  password:['',[Validators.minLength(6)]]
-});
+  formBuilder = inject(FormBuilder);
+  loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.minLength(6)]]
+  });
 
-login(){
-  this.authService.loginUser(this.loginForm.value).subscribe({
-    next:(result:any)=>{
-        console.log("Login result: ",result)
-      this.toaster.success("Login Success","Success");
-      
-    },
-    error:(err:any)=>{
-      this.toaster.error("Login Failed","Failed");
+  login() {
+    this.authService.loginUser(this.loginForm.value).subscribe({
+      next: (result: any) => {
+        console.log("Login result: ", result.data.token)
+        this.cookieService.set('session-token', result.data.token);
+        this.toaster.success("Login Success", "Success");
+      },
+      error: (err: any) => {
+        this.toaster.error("Login Failed", "Failed");
 
-    },
-    complete:()=>{}
-  })
-}
+      },
+      complete: () => { }
+    })
+  }
 
-showSuccess() {
-}
-  
+  showSuccess() {
+  }
+
 }
