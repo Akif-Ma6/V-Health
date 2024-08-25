f243b739476549b7f89fd6d9149adbd7921f0100
Arun Anand <107839202+ArunAnand22@users.noreply.github.com>
2024-08-25 14:49:18 +0530

    sidebar improvement

--- client/src/app/layout/base/base.component.ts
+++ client/src/app/layout/base/base.component.ts
@@ -3,16 +3,18 @@ import { Router,RouterOutlet,RouterModule } from '@angular/router';
 
 import { NavbarComponent } from '../navbar/navbar.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-base',
   standalone: true,
-  imports: [RouterOutlet,RouterModule,NavbarComponent,SidebarComponent],
+  imports: [RouterOutlet,RouterModule,NavbarComponent,SidebarComponent,FooterComponent],
   templateUrl: './base.component.html',
   styleUrl: './base.component.scss'
 })
 export class BaseComponent {
 
   // router = inject(Router)
+  
 
 }
--- client/src/app/layout/navbar/navbar.component.ts
+++ client/src/app/layout/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+  router = inject(Router)
+
+  logout() {
+    // Clear user session or authentication token
+    // Redirect to login page or home page
+    this.router.navigate(['/login']); // Adjust the route as needed
+  }
+
+  goToProfile() {
+    // Redirect to profile page
+    this.router.navigate(['/profile']); // Adjust the route as needed
+  }
 
 }
